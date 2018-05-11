e4b1294657ce8fc5fa9727f701c2d903a7c24c97
Tonmoy Sardar <tonmoy.1984@gmail.com>
2018-05-11 12:37:40 +0530

    company add part 1

--- src/app/layout/company/company-add/company-add.component.ts
+++ src/app/layout/company/company-add/company-add.component.ts
@@ -1,19 +1,74 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { CompanyService } from '../company.service';
+import { StatesService} from '../../states/states.service';
+
 @Component({
   selector: 'app-company-add',
   templateUrl: './company-add.component.html',
   styleUrls: ['./company-add.component.scss']
 })
 export class CompanyAddComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  company;
+  companyList;
+  stateList;
+  constructor(private companyService: CompanyService, private statesService: StatesService, private router: Router) { }
 
   ngOnInit() {
+    this.company = {
+      id: '',
+      company:0,
+      company_name: '',
+      company_url: '',
+      company_email: '',
+      company_contact: '',
+      company_address:'',
+      company_state:'',
+      company_city:'',
+      company_pin:'',
+      company_gst:'',
+      company_pan:'',
+      company_cin:''
+    };
+    this.getCompanyDropdownList();
+    this.getStateList();
   }
+
+
+  goToList= function (toNav) {
+    this.router.navigateByUrl('/'+toNav);
+  };
+
+  addNewCompany = function(){
+    this.companyService.addNewCompany(this.company).subscribe(
+      response => {
+        this.goToList('company');
+      },
+      error => console.log('error',error)
+    );
+  }
+
+  getStateList= function(){
+    this.statesService.getStateList().subscribe(
+      (data: any[]) =>{   
+        this.stateList = data['results'];
+        
+      }
+     );
+  };
+
+  getCompanyDropdownList= function(){
+    this.companyService.getCompanyDropdownList().subscribe(
+      (data: any[]) =>{   
+        this.companyList = data['results'];
+        console.log(this.companyList);
+      }
+     );
+  };
 
   btnClickNav= function (toNav) {
     this.router.navigateByUrl('/'+toNav);
   };
+
 }
