9a4ce0057d3153321d15ee6f39e55483e664ed8c
Biswajit Manna <404manna@gmail.com>
2018-05-11 12:18:25 +0530

    state search done

--- src/app/layout/states/states-edit/states-edit.component.ts
+++ src/app/layout/states/states-edit/states-edit.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { StatesService} from '../states.service';
-
-
+import { StatesService } from '../states.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-states-edit',
   templateUrl: './states-edit.component.html',
@@ -11,44 +11,85 @@ import { StatesService} from '../states.service';
 export class StatesEditComponent implements OnInit {
   states;
   state;
-  constructor(private statesService: StatesService, private router: Router, private route: ActivatedRoute) { }
+  form: FormGroup;
+  constructor(
+    private statesService: StatesService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit() {
     this.states = {
       id: '',
       state_name: '',
       tin_number: '',
-      state_code:''
+      state_code: ''
     };
+    this.form = this.formBuilder.group({
+      state_name: [null, Validators.required],
+      tin_number: [null, Validators.required],
+      state_code: [null, Validators.required]
+    });
     this.getStateDetails(this.route.snapshot.params['id']);
   }
 
-  getStateDetails = function(id) {
-
+  getStateDetails = function (id) {
     this.statesService.getStateDetails(id).subscribe(
-      (data: any[]) =>{  
+      (data: any[]) => {
         this.states = data;
       }
-     );
-  }
-
-  goToList= function (toNav) {
-    this.router.navigateByUrl('/'+toNav);
-  };
-
-
-  updateState = function(){
-    this.statesService.updateState(this.states).subscribe(
-      response => {
-        this.goToList('states');
-      },
-      error => console.log('error',error)
     );
   }
 
-  btnClickNav= function (toNav) {
-    this.router.navigateByUrl('/'+toNav);
+  goToList = function (toNav) {
+    this.router.navigateByUrl('/' + toNav);
   };
+
+
+  updateState = function () {
+    if (this.form.valid) {      
+      this.statesService.updateState(this.states).subscribe(
+        response => {
+          this.toastr.success('State updated successfully', '', {
+            timeOut: 3000,
+          });
+          this.goToList('states');
+        },
+        error => {
+          console.log('error', error)
+          // this.toastr.error('everything is broken', '', {
+          //   timeOut: 3000,
+          // });
+        }
+      );
+    } else {
+      Object.keys(this.form.controls).forEach(field => {
+        const control = this.form.get(field);
+        control.markAsTouched({ onlySelf: true });
+      });
+    }    
+  }
+
+  btnClickNav = function (toNav) {
+    this.router.navigateByUrl('/' + toNav);
+  };
+
+  reSet() {
+    this.form.reset();
+  }
+
+  isFieldValid(field: string) {
+    return !this.form.get(field).valid && this.form.get(field).touched;
+  }
+
+  displayFieldCss(field: string) {
+    return {
+      'is-invalid': !this.form.get(field).valid && this.form.get(field).touched,
+      'is-valid': this.form.get(field).valid
+    };
+  }
 }
 
 
