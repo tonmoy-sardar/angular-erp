b9ce44e4c01ae65684a519099810f2f19214f7c9
Biswajit Manna <biswajit.manna@shyamsteel.com>
2018-05-14 15:19:34 +0530

    purchase requisition

--- src/app/layout/purchase-requisition/purchase-requisition-add/purchase-requisition-add.component.ts
+++ src/app/layout/purchase-requisition/purchase-requisition-add/purchase-requisition-add.component.ts
@@ -1,20 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { FormGroup, Validators, FormBuilder, FormArray } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-purchase-requisition-add',
   templateUrl: './purchase-requisition-add.component.html',
   styleUrls: ['./purchase-requisition-add.component.scss']
 })
 export class PurchaseRequisitionAddComponent implements OnInit {
-
-  constructor(private router: Router) { }
-
+  form: FormGroup;
+  items: FormArray;
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) { }
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      purchase_organization: ['', Validators.required],
+      purchase_group: ['', Validators.required],
+      company: ['', Validators.required],
+      date: ['', Validators.required],
+      note: ['', Validators.required],
+      items: this.formBuilder.array([ this.createItem() ])
+    });
+  }
+  createItem() {
+    return this.formBuilder.group({
+      material: ['', Validators.required],
+      quantity: ['', Validators.required],
+      UMO: ['', Validators.required],
+      plant: ['', Validators.required],
+      storage: ['', Validators.required],
+      bin: ['', Validators.required],
+      vendor: ['', Validators.required],
+      vendor_address: ['', Validators.required]
+    });
   }
 
+  addItem(){
+    this.items = this.form.get('items') as FormArray;
+    this.items.push(this.createItem());
+  }
   btnClickNav= function (toNav) {
     this.router.navigateByUrl('/'+toNav);
   };
+
+  addPurchaseRequisition () {
+    console.log(this.form.value)
+    if (this.form.valid) {
+      
+    } else {
+      Object.keys(this.form.controls).forEach(field => {
+        const control = this.form.get(field);
+        control.markAsTouched({ onlySelf: true });
+      });
+    }
+
+  }
+
+  reSet() {
+    this.form.reset();
+  }
+  isFieldValid(field: string) {
+    return !this.form.get(field).valid && this.form.get(field).touched;
+  }
+  displayFieldCss(field: string) {
+    return {
+      'is-invalid': this.form.get(field).invalid && (this.form.get(field).dirty || this.form.get(field).touched),
+      'is-valid': this.form.get(field).valid && (this.form.get(field).dirty ||this.form.get(field).touched)
+    };
+  }
 
 }
--- src/app/layout/purchase-requisition/purchase-requisition.service.ts
+++ src/app/layout/purchase-requisition/purchase-requisition.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
+import { environment } from '../../../environments/environment';
 @Injectable()
 export class PurchaseRequisitionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // getPurchaseRequisitionList(params): Observable<any>{
+  //   return this.http.get(environment.apiEndpoint+'states/?'+params, {
+  //     headers: new HttpHeaders().set('Authorization', 'Token '+localStorage.getItem('logedUserToken'))
+  //   })
+  // }
 
 }
